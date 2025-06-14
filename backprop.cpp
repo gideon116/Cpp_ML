8ce61401a0b8be41c323c931fd6e997caee60335
Gideon Kassa <76665853+gideon116@users.noreply.github.com>
2025-06-14 17:38:20 -0400

    added Layer class

--- backprop.cpp
+++ backprop.cpp
@@ -1,105 +1,58 @@
 #include <iostream>
 #include <random>
 #include "matrix_operations.h"
-
+#include "layers.h"
 
 using matrix2D = std::vector<std::vector<double>>;
 using matrixType = std::vector<matrix2D>;
 
 int main() {
 
-    matrixOperations o;
+    matrixOperations wf;
 
     matrixType input = { // batch, 2, 3
         {{3.5, 3.69, 3.44}, {4.34, 4.42, 2.37}},
         {{3.5, 3.69, 3.43}, {4.34, 4.42, 2.37}},
     };
 
-    matrixType real = { // batch, 2, 1
-        {{18}, {3}},
-        {{1}, {17}},
+    matrixType real = { // batch, 2, 2
+        {{18, 3}, {3, 3}},
+        {{1, 17}, {1, 3}},
     };
 
-    int batch = o.shape(input)[0];
-    int rows = o.shape(input)[1];
-    int cols = o.shape(input)[2];
+    int batch = wf.shape(input)[0];
+    int rows = wf.shape(input)[1];
+    int cols = wf.shape(input)[2];
 
-    matrix2D w1( // 3, 2
-        cols, std::vector<double>(rows)
-    );
-
-    matrix2D w2( // 2, 1
-        rows, std::vector<double>(1)
-    );
-
-    // random init for weights
-    std::mt19937 gen(42);
-    std::normal_distribution<double> dist(0.0, 1.0/std::sqrt(cols));
-    for (std::vector<double>& d1 : w1) {
-        for (double& d2 : d1) {
-            d2 = dist(gen);
-        }
-    }
-    for (std::vector<double>& d1 : w2) {
-        for (double& d2 : d1) {
-            d2 = dist(gen);
-        }
-    }
-
-
-    matrix2D w2T;
-    matrixType inputT;
-    matrixType aT;
-
-    matrixType pred;
-    matrixType dpred;
-    matrix2D dw1;
-    matrixType dlayer1;
-    matrix2D dw2;
-
-    matrixType layer1;
-    matrixType a;
-    matrixType da;
     double loss;
-
     double lr = 0.01;
 
-    for (int i = 0; i < 100; i++) {
-        layer1 = o.matmul(input, w1);  // b, 2, 2
-        a = o.relu(layer1);
-        pred = o.matmul(a, w2);  // b, 2, 1
+    Linear layer1a(cols, rows), layer2a(rows, 1), layer3a(1, rows);
+    ReLU relu1, relu2;
+    std::vector<Layer*> network = {&layer1a, &relu1, &layer2a, &relu2, &layer3a};
+
+    for (int epoch = 0; epoch < 100; epoch++) {
+
+        matrixType y = input;
+
+        for (Layer* layer : network) {
+            y = (*layer).forward_pass(y, wf);
+        }
+
+        matrixType dy = wf.constMul(
+            2.0 / (batch * rows * cols),
+            wf.subtract(y, real)
+        );
+
+        for (int i = static_cast<int>(network.size()) - 1; i >= 0; i--) {
+            dy = (*network[i]).backward_pass(dy, lr, wf);
+        }
         
-        // backpropagation
-        dpred = o.subtract(pred, real);  // b, 2, 1
-        dpred = o.constMul(2.0 / (batch * rows), dpred);  // b, 2, 1
-
-        aT = o.transpose(a);  // b, 2, 2
-        aT = o.matmul(aT, dpred);  // b, 2, 1
-        dw2 = o.sumBatch(aT);  // 2, 1
-
-        w2T = o.transpose(w2);  // 1, 2
-        da = o.matmul(dpred, w2T);  // b, 2, 2
-        dlayer1 = o.d_relu(a);
-        dlayer1 = o.elemwise(dlayer1, da);
-
-        inputT = o.transpose(input);
-        inputT = o.matmul(inputT, dlayer1);
-        dw1 = o.sumBatch(inputT);
-
-        // update weights
-        dw1 = o.constMul(lr, dw1);
-        dw2 = o.constMul(lr, dw2);
-
-        w1 = o.subtract(w1, dw1);
-        w2 = o.subtract(w2, dw2);
-
         // loss calc
-        loss = o.l2(pred, real);
-
-        std::cout << "epoch " << i << std::endl;
+        loss = wf.l2(y, real);
+        std::cout << "epoch " << epoch << std::endl;
         std::cout << loss << std::endl;
         
-
     }
 
     return 0;
